62b49c8c7f2355c2b7cca7bc2e93234c9b1161e6
Justus <justus.d2025@gmail.com>
2025-06-12 15:54:52 +0200

    added jff & changed the root

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask
 
 app = Flask(__name__)
+from flask import render_template
+from json import load
 
-
+def jff(path: str) -> list | dict:
+    """ returns a json file """
+    with open(path,'r') as f_in:
+        return load(f_in)
+blog_posts = jff('blog.json')
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    # add code here to fetch the job posts from a file
+    return render_template('index.html', posts=blog_posts)
+
 
 
 if __name__ == '__main__':
