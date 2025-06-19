c8fd168fce6619d6ecf0fe216af2041cd7f7ea9e
Justus <justus.d2025@gmail.com>
2025-06-19 10:21:44 +0200

    m

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template, request
 
-from json import load
+from json import load, dumps
 
 app = Flask(__name__)
 
@@ -10,9 +10,22 @@ def jff(path: str) -> list | dict:
     with open(path,'r') as f_in:
         return load(f_in)
 
+def jsf(path: str, data: dict | list) -> None:
+    """ write json to a file """
+    with open(path,'w') as f_out:
+        f_out.write(dumps(data,indent=2))
+
 class BlogPost:
     def __init__(self, data):
         self.data = data
+    
+    def asdict(self) -> dict:
+        return {
+            'title': self.title,
+            'author': self.author,
+            'content': self.content
+        }
+    
     @property
     def title(self) -> str:
         return self.data['title']
@@ -23,55 +36,67 @@ class BlogPost:
     def content(self) -> str:
         return self.data['content']
 
-blog_posts = [BlogPost(i) for i in jff('blog.json')]
+class Blog:
+    def __init__(self):
+        self.load()
+    
+    def save(self):
+        jsf('blog.json',self.aslist())
+    
+    def load(self):
+        self.data = [BlogPost(i) for i in jff('blog.json')]
+    
+    def aslist(self) -> list:
+        return [i.asdict() for i in self.data]
+    
+    def add(self, post: BlogPost) -> None:
+        self.data.insert(0,post)
+    
+    def delete(self, post_id: int) -> None:
+        self.data.pop(post_id)
+    
+    @property
+    def length(self) -> int:
+        return len(self.data)
+
+BLOG = Blog()
 @app.route('/')
 def index():
     overwrite = ""
-    for blog_post in blog_posts:
-        overwrite += f"""
-<div class="post">
-    <h2>{blog_post.title}</h2>
-    <p><em>{blog_post.author}</em></p>
-    <p>{blog_post.content}</p>
-</div>
-
-        """
+    
     return render_template('index.html', posts=blog_posts).replace('__BLOG_POSTS__', overwrite)
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
-        # We will fill this in the next step
-        new_post = {"id": len(blog_posts)+1,
+        new_post = {"id": BLOG.length+1,
          "author":request.form['name'],
          "title": request.form['title'],
          "content": request.form['content']}
-        blog_posts.append(new_post)
-        
+        BLOG.add(BlogPost(new_post))
         
     return render_template('add.html')
 
 @app.route('/delete/<int:post_id>')
 def delete(post_id):
-    blog_posts.pop(post_id)
-    return render_template('index.html', posts=blog_posts)
+    BLOG.delete(post_id)
+    return render_template('index.html')
 
 
 @app.route('/update/<int:post_id>', methods=['GET', 'POST'])
 def update(post_id):
     # Fetch the blog posts from the JSON file
-    post = blog_posts.index()
-    if post is None:
-        # Post not found
-        return "Post not found", 404
-    
+    if post_id >= BLOG.length:
+        return 'cant find post', 404
+    if post_id < 0:
+        return 'negative type not supported', 404
     if request.method == 'POST':
         # Update the post in the JSON file
         # Redirect back to index
         request
     # Else, it's a GET request
     # So display the update.html page
-    return render_template('update.html', post=post)
+    return render_template('update.html', post=BLOG)
 
 
 if __name__ == '__main__':
